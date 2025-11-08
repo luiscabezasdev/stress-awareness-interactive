8dcb904d09d8a3c76db1786f648e34dc0b9e8d1a
luiscabezasdev <luiscabezas25@hotmail.com>
2025-11-08 17:23:24 -0500

    despliegue

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = 'stress-awareness-interactive'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
   plugins: [react()],
   server: {
     host: process.env.HOST ?? '0.0.0.0',
