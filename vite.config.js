851ce28805b17229d31275d050560ce762da3d6a
luiscabezasdev <luiscabezas25@hotmail.com>
2025-11-08 15:40:26 -0500

    validación npm modules

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   server: {
     host: process.env.HOST ?? '0.0.0.0',
     port: Number(process.env.PORT) || 4173,
